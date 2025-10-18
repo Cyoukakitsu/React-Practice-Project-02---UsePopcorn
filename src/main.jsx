1675d8d334f5c653e9293a31da7a45e316ddaada
ZHAO JIAYI <z261459808@gmail.com>
2025-10-18 17:38:24 +0900

    next push

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import App from "./App.jsx";
 import StarRating from "./StarRating.jsx";
+import { useState } from "react";
+
+export function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test />
   </StrictMode>
 );
